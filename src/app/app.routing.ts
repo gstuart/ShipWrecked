03a7aa457e5b8d99620b4f3ce9b29be8aa161fed
jahan <jwalsh370@gmail.com>
2017-05-09 09:27:44 -0700

    add info for light component create buttons for giveUp and gun.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,6 +5,7 @@ import { BeachComponent } from './beach/beach.component';
 import { JungleComponent } from './jungle/jungle.component';
 import { HillComponent } from './hill/hill.component';
 import { AboutComponent } from './about/about.component';
+import { LightComponent } from './light/light.component';
 
 const appRoutes: Routes = [
   {
@@ -26,7 +27,12 @@ const appRoutes: Routes = [
   {
     path: 'about',
     component: AboutComponent
+  },
+  {
+    path: 'light',
+    component: LightComponent
   }
+
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
